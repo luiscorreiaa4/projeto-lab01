973ca1696face6262808b17d650b92ae7dc1aa99
unknown <pleluis@gmail.com>
2025-05-15 15:16:09 -0300

    initial commit

---
+++ main.h
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+void print_rock_left() {
+  printf("    _______\n");
+  printf("---'   ____)\n");
+  printf("      (_____)\n");
+  printf("      (_____)\n");
+  printf("      (____)\n");
+  printf("---.__(___)\n");
+}
+
+void print_scissors_left() {
+  printf("    _______\n");
+  printf("---'   ____)____\n");
+  printf("          ______)\n");
+  printf("       __________)\n");
+  printf("      (____)\n");
+  printf("---.__(___)\n");
+}
+
+void print_paper_left() {
+  printf("    _______\n");
+  printf("---'   ____)____\n");
+  printf("          ______)\n");
+  printf("          _______)\n");
+  printf("         _______)\n");
+  printf("---.__________)\n");
+}
+
+void print_rock_right() {
+  printf("         _______\n");
+  printf("        (____   '---\n");
+  printf("      (_____)\n");
+  printf("      (_____)\n");
+  printf("       (____)\n");
+  printf("        (___)__.---\n");
+}
+
+void print_scissors_right() {
+  printf("        _______\n");
+  printf("   ____(____   '---\n");
+  printf("  (______\n");
+  printf(" (__________\n");
+  printf("      (____)\n");
+  printf("        (___)__.---\n");
+}
+
+void print_paper_right() {
+  printf("       _______\n");
+  printf("  ____(____   '---\n");
+  printf(" (______\n");
+  printf("(_______\n");
+  printf("(_______\n");
+  printf("  (__________.---\n");
+}
